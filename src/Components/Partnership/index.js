8824f590048d71e1f0c625783e0a9b8359f633b1
Ombito <alvinombito09@gmail.com>
2024-06-19 00:23:21 +0300

    Fix: partnership

--- src/Components/Partnership/index.js
+++ src/Components/Partnership/index.js
@@ -3,44 +3,74 @@ import './partnership.css';
 import partner1 from '../../Assets/aws.jpg';
 import partner2 from '../../Assets/cisco.jpg';
 import partner3 from '../../Assets/huawei.jpg';
-import partner4 from '../../Assets/fortinet.jpg';
-import partner5 from '../../Assets/hp.jpg';
+import partner4 from '../../Assets/fortinet1.jpg';
+import partner5 from '../../Assets/microsoft.jpg';
 import partner6 from '../../Assets/veeam.jpg';
-import partner7 from '../../Assets/microsoft.jpg';
+import partner7 from '../../Assets/hp.jpg';
 import partner8 from '../../Assets/odoo.jpg';
+import partner9 from '../../Assets/sophos.jpg';
+import partner10 from '../../Assets/lenovo.jpg';
+import partner11 from '../../Assets/mcafee.jpg';
+import partner12 from '../../Assets/hikvision.jpg';
+import partner13 from '../../Assets/redhat.png';
+import partner14 from '../../Assets/dell.jpg';
 
 const partners = [
   {
-    name: 'Partner A',
+    name: 'AWS',
     image: partner1,
   },
   {
-    name: 'Partner B',
+    name: 'Cisco',
     image: partner2,
   },
   {
-    name: 'Partner C',
+    name: 'Huawei',
     image: partner3,
   },
   {
-    name: 'Partner D',
+    name: 'Fortinet',
     image: partner4,
   },
   {
-    name: 'Partner E',
+    name: 'Microsoft',
     image: partner5,
   },
   {
-    name: 'Partner E',
+    name: 'Veeam',
     image: partner6,
   },
   {
-    name: 'Partner E',
+    name: 'HP',
     image: partner7,
   },
   {
-    name: 'Partner E',
+    name: 'Odoo',
     image: partner8,
+  },
+  {
+    name: 'Sophos',
+    image: partner9,
+  },
+  {
+    name: 'Lenovo',
+    image: partner10,
+  },
+  {
+    name: 'McAfee',
+    image: partner11,
+  },
+  {
+    name: 'Hikvision',
+    image: partner12,
+  },
+  {
+    name: 'RedHat',
+    image: partner13,
+  },
+  {
+    name: 'Dell',
+    image: partner14,
   },
 ];
 
