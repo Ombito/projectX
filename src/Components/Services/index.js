060a6499f98f1ded106f43d845576c154c375e00
Ombito <alvinombito09@gmail.com>
2024-06-18 19:33:09 +0300

    Fix: services

--- src/Components/Services/index.js
+++ src/Components/Services/index.js
@@ -11,25 +11,16 @@ const Services = () => {
         <p>Delivering Excellence in Every Solution</p>
       </div>
       <div className="intro-section">
-        <h2>Welcome to Hazel Systems</h2>
-        <p>At Hazel Systems, we are dedicated to providing top-notch services that cater to the unique needs of your business. Our team of experts works tirelessly to ensure that we deliver solutions that drive growth, enhance efficiency, and secure your operations.</p>
+        <h2>Welcome to Milmax Systems</h2>
+        <p>At Milmax Systems, we are dedicated to providing outstanding services that cater to the unique needs of your business. Our team of experts works tirelessly to ensure that we deliver solutions that drive growth, enhance efficiency, and secure your operations.</p>
       </div>
       <div className="services-list">
         <div className="service-item" style={{ backgroundImage: `url(${logo1})` }}>
           <div className="service-content">
-            <h3>Application Services</h3>
+            <h3>Business Applications</h3>
             <p>Delivering custom software solutions to meet your business needs.</p>
             <Link to={`/services`} className="arrow-link">
                 <span>&rarr;</span>
-            </Link>
-          </div>
-        </div>
-        <div className="service-item" style={{ backgroundImage: `url(${logo1})` }}>
-          <div className="service-content">
-            <h3>Business Consulting</h3>
-            <p>Providing strategic advice to drive growth and efficiency.</p>
-            <Link to={`/services`} className="arrow-link">
-              <span>&rarr;</span>
             </Link>
           </div>
         </div>
@@ -44,7 +35,7 @@ const Services = () => {
         </div>
         <div className="service-item" style={{ backgroundImage: `url(${logo1})` }}>
           <div className="service-content">
-            <h3>Cybersecurity</h3>
+            <h3>System Security</h3>
             <p>Protecting your business with advanced security solutions.</p>
             <Link to={`/services`} className="arrow-link">
               <span>&rarr;</span>
@@ -80,8 +71,17 @@ const Services = () => {
         </div>
         <div className="service-item" style={{ backgroundImage: `url(${logo1})` }}>
           <div className="service-content">
-            <h3>Integration</h3>
+            <h3>System Integration</h3>
             <p>Seamlessly integrating diverse systems for enhanced performance.</p>
+            <Link to={`/services`} className="arrow-link">
+              <span>&rarr;</span>
+            </Link>
+          </div>
+        </div>
+        <div className="service-item" style={{ backgroundImage: `url(${logo1})` }}>
+          <div className="service-content">
+            <h3>Business Consulting</h3>
+            <p>Providing strategic advice to drive growth and efficiency.</p>
             <Link to={`/services`} className="arrow-link">
               <span>&rarr;</span>
             </Link>
@@ -108,7 +108,7 @@ const Services = () => {
         </div>
         <div className="faq">
           <h3>How can I get in touch?</h3>
-          <p>You can reach us through the contact form, or email us at contact@hazelsystems.com</p>
+          <p>You can reach us through the contact form, or email us at contact@Milmaxsystems.com</p>
         </div>
       </div>
     </div>
