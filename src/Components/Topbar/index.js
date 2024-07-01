a48b0aab8bd15fd7b962890518147e36a2532364
Alvin Ombito <alvinombito09@gmail.com>
2024-07-01 18:42:43 +0300

    Feat: add topbar

---
+++ src/Components/Topbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './topbar.css';
+
+const Topbar = () => {
+  return (
+    <div>
+        <div class="top-bar">
+        <div class="contact-info">
+            <span class="phone">Talk to Us: +254-721-857-213</span>
+            <span class="email">info@milmax-systems.com</span>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Topbar;
